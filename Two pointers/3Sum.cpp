5b036e3136ade2e6099eab74b4f024680f67fbc8
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-04-24 11:25:44 +0500

    optimzed way to solve Three Sum problem

--- Two pointers/3Sum.cpp
+++ Two pointers/3Sum.cpp
@@ -1,18 +1,66 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-class Solution {
+// ----------------------------------------------Approach(Brute Force)----------------------------------------------------------------
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         vector<vector<int>> result;
+//         //O(n^3) TC
+//         for (int i = 0; i < nums.size(); i++) { // n*n*n
+//             for (int j = i + 1; j < nums.size(); j++) {
+//                 for (int k = j + 1; k < nums.size(); k++) {
+//                     if (nums[i] + nums[j] + nums[k] == 0) {
+//                         result.push_back({nums[i], nums[j], nums[k]});
+//                     }
+//                 }
+//             }
+//         }
+
+//         return result;
+//     }
+// };
+
+// ----------------------------------------------Approach(optimized way)----------------------------------------------------------------
+
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
         vector<vector<int>> result;
-        //O(n^3) TC
-        for (int i = 0; i < nums.size(); i++) { // n*n*n
-            for (int j = i + 1; j < nums.size(); j++) {
-                for (int k = j + 1; k < nums.size(); k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        result.push_back({nums[i], nums[j], nums[k]});
-                    }
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            //skip duplicate indexes
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0)
+                {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[left+1]) left++;
+                    while (left < right && nums[right] == nums[right-1]) right--;
+                    left++;
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
                 }
             }
         }
@@ -21,18 +69,20 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     cout << "3 Sum problem" << endl;
 
     vector<int> testcase1 = {-1, 0, 1, 2, -1, -4};
 
     Solution sol;
-    vector<vector<int>> result = sol.threeSum(testcase1); 
+    vector<vector<int>> result = sol.threeSum(testcase1);
 
-
-    for (const auto& triplet : result) {
+    for (const auto &triplet : result)
+    {
         cout << "[";
-        for (int num : triplet) {
+        for (int num : triplet)
+        {
             cout << num << " ";
         }
         cout << "]" << endl;
